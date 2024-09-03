840affad3f7b2297afe479fefe47a639d9cc2d38
Janvi <mungekarjanvi@gmail.com>
2024-09-03 13:00:33 +0530

    added search routes

--- routes/stationRoutes.js
+++ routes/stationRoutes.js
@@ -3,8 +3,10 @@ const StationController = require('../controllers/stationController');
 
 const router = express.Router();
 
+// Define routes
 router.post('/', StationController.createStation); // Create a station
 router.get('/', StationController.getAllStations); // Get all stations with pagination
+router.get('/search', StationController.searchStations); // Search stations by query
 router.get('/:id', StationController.getStationById); // Get a station by ID
 router.put('/:id', StationController.updateStation); // Update a station by ID
 router.delete('/:id', StationController.deleteStation); // Delete a station by ID
